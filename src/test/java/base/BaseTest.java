0811ba9f5d0dbf61514622a56e8ff83fbfbadec5
HieuTran <graynatsu80@gmail.com>
2025-03-02 18:56:39 +0700

    Update action in BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import config.DriverConfig;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import utils.Tools;
@@ -11,13 +12,14 @@ import utils.Tools;
 public class BaseTest extends DriverConfig {
     protected Tools tools;
     protected JavascriptExecutor js;
-
+    protected Actions actions;
     @BeforeSuite
     protected void setupSuite() {
         driver = getDriver();
         driver.get(baseURL);
         tools = new Tools(driver);
         js = (JavascriptExecutor) driver;
+        actions = new Actions(driver);
     }
 
     @AfterSuite
