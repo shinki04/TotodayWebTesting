e1fa28bec2dad5d387ce615756878cce40113215
Nhinguyen <238nhinguyen@gmail.com>
2025-03-03 16:15:10 +0700

    LoginTest ver2

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -8,6 +8,11 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.ExcelReader;
+import utils.Notification;
+
+import java.io.IOException;
+import java.util.List;
 
 public class LoginTest extends BaseTest {
     private String loginURL = baseURL + "/user/signin";
@@ -21,23 +26,30 @@ public class LoginTest extends BaseTest {
     @BeforeMethod
     public void setupTMethod() {
         driver.get(loginURL);
-
+        excelReader = new ExcelReader("./src/test/resources/login.xlsx");
     }
 
     @Test(priority = 0)
-    public void testSuccess() throws InterruptedException {
-
+    public void testSuccess() throws InterruptedException, IOException {
         driver.manage().window().maximize();
 
-        // Nhập thông tin đăng nhập
-        driver.findElement(By.id("SignInEmail")).sendKeys("innologic25.team@gmail.com");
-        driver.findElement(By.id("password-field")).sendKeys("innologic2025");
+        // Đọc dữ liệu từ file Excel (giả sử thông tin đăng nhập nằm ở Sheet 0, dòng đầu tiên)
+        List<String[]> loginData = excelReader.readExcelData(0);
+        String email = loginData.get(0)[0]; // Cột 1: Email
+        String password = loginData.get(0)[1]; // Cột 2: Mật khẩu
+
+        driver.get(loginURL);
+
+        // Nhập thông tin đăng nhập từ file Excel
+        driver.findElement(By.id("SignInEmail")).sendKeys(email);
+        driver.findElement(By.id("password-field")).sendKeys(password);
 
         // Gửi biểu mẫu đăng nhập
         driver.findElement(By.xpath("//button[@type='submit'][contains(text(),'Đăng nhập')]")).click();
-        sleep(2);
+        Thread.sleep(2000);
 
         // Kiểm tra nếu có alert xuất hiện
+        Notification notification = new Notification(driver);
         if (notification.isAlertPresent()) {
             String actualMessage = notification.getAlertText();
             System.out.println("Thông báo từ hệ thống: " + actualMessage);
@@ -57,10 +69,10 @@ public class LoginTest extends BaseTest {
 
         System.out.println("Đăng nhập thành công! Hiện tại ở URL: " + currentURL);
 
-        // Nhấn vào đường dẫn đăng xuất
+//         Nhấn vào đường dẫn đăng xuất
+        sleep(5);
         driver.findElement(By.xpath("//img[@alt='Tài khoản']")).click();
         driver.findElement(By.xpath("//a[@href='/user/signout']")).click();
-
     }
 
     @Test(priority = 1)
