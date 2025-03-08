904bd2c17afa2dd187a410625ef85d3743fab310
Nhinguyen <238nhinguyen@gmail.com>
2025-03-08 23:59:19 +0700

    Doing Edit Pass and bug element

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,9 +2,9 @@ package base;
 
 import config.DriverConfig;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import utils.ExcelReader;
 import utils.Notification;
@@ -14,21 +14,19 @@ import utils.Tools;
 
 
 public class BaseTest extends DriverConfig {
+    protected static WebDriver driver;
     protected Tools tools;
     protected JavascriptExecutor js;
-
     protected Notification notification;
     protected ExcelReader excelReader;
     protected PopupHandler popupHandler;
-
-
-
     protected Actions actions;
 
-    @BeforeMethod
+    @BeforeSuite
     protected void setupSuite() {
         driver = getDriver();
         driver.get(baseURL);
+        driver.manage().window().maximize();
         tools = new Tools(driver);
         js = (JavascriptExecutor) driver;
         notification = new Notification(driver);
